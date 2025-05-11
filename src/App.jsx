327da70b206cdddf50a5d01b16fcedfcc67f7ee0
kyBa-Le <lekyba2000hc@gmail.com>
2025-05-11 16:42:47 +0700

    learning conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
 import { useState } from 'react'
 import './App.css'
 
+function Item({name, isPacked}) {
+  return <li className='item'>{name} {isPacked && '✅'}</li>
+}
+
+export function PackingList() {
+  return (
+    <section>
+      <h1>Sally Ride's Packing List</h1>
+      <ul>
+        <Item
+          isPacked={true}
+          name="Space suit"
+        />
+        <Item
+          isPacked={true}
+          name="Helmet with a golden leaf"
+        />
+        <Item
+          isPacked={false}
+          name="Photo of Tam"
+        />
+      </ul>
+    </section>
+  );
+}
+
 function App() {
   return (
     <>
+      <PackingList></PackingList>
     </>
   )
 }
